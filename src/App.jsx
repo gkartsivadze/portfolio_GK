a8c72889c864e2d04c5721ed1ba8f35c4f9f9bb6
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-07-29 13:24:06 +0400

    rearrangment of portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
 import React, { useEffect } from "react"
 
-import Navigation from "./components/Navigation"
-import Content from "./components/Content"
-
 import { gsap } from "gsap"
 import { TimelineMax } from "gsap/gsap-core"
 
 function App() {
   useEffect(() => {
-    let tl = new TimelineMax();
-    tl.to("#root", 5, { "--x": "15%", "--y": "15%"})
-      .to("#root", 5, {"--x": "80%", "--y": "45%"})
-      .to("#root", 5, {"--x": "45%", "--y": "90%"})
-      .yoyo(true).repeat(-1)
+    gsap.fromTo(".horizontal_line", 2,{width: "0%"}, {width: "100%"})
   }, [])
+
+  function toProjects() {
+    gsap.to("#welcome_section", 0.5, {background: "none"})
+    gsap.to("#welcome_section .container", 0.5, {translateZ: "1rem", scale:1.1, opacity: 0, onComplete() {gsap.set("#welcome_section", {display: "none"})}})
+    gsap.to("#projects_section .container", 0.5, {scale: 1.1, filter: "none"})
+
+    document.addEventListener("mousemove", handleMovement);
+  }
+
+  function handleMovement(e) {
+    // gsap.to("projects_section .container", {transform: x})
+  }
+
   return (
-    <>
-      <Navigation />
-      <Content />
-    </>
+    <main>
+      <section id="welcome_section">
+        <div className="container">
+        <div className="hero_wrapper">
+          <h1>GIORGI KARTSIVADZE</h1>
+          <div className="horizontal_line"></div>
+          <p>Front-end Developer</p>
+        </div>
+        <button onClick={toProjects}>SHOWROOM</button>
+        </div>
+      </section>
+      <section id="projects_section">
+        <div className="container">
+          <div className="project_card">1</div>
+          <div className="project_card">2</div>
+          <div className="project_card">3</div>
+          <div className="project_card">4</div>
+          <div className="main">It's main</div>
+          <div className="project_card">5</div>
+          <div className="project_card">6</div>
+          <div className="project_card">7</div>
+          <div className="project_card">8</div>
+          <div className="project_card">9</div>
+          <div className="project_card">10</div>
+          <div className="project_card">11</div>
+          <div className="project_card">12</div>
+        </div>
+      </section>
+    </main>
   )
 }
 
