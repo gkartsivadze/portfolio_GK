a0fbcf0969480a2422656b9a2b4ac36c5ff49726
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-07-29 17:19:31 +0400

    Added navigation. Improved styling

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navigation() {
+    return (
+        <nav id="navigation">
+          <button>Projects</button>
+          <button>Contact</button>
+        </nav>
+    )
+};
