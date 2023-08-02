c4283f34a0979f9d3c500a0681b959c660288fc0
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-08-02 17:43:23 +0400

    Added contact form animations

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,7 +5,7 @@ export default function ContactForm({ setContactForm }) {
     const sectionRef = useRef();
     const formRef = useRef();
     function closeForm(e) {
-        e.preventDefault();
+        e?.preventDefault();
 
         sectionRef.current.classList.add("hide")
         setTimeout(() => {
@@ -17,11 +17,19 @@ export default function ContactForm({ setContactForm }) {
     function sendEmail(e) {
         e.preventDefault();
 
+        document.getElementById("send_button").classList.add("loading");
+        document.getElementById("send_button").setAttribute("disabled", true)
+        document.getElementById("send_button").innerText = ""
         emailjs.sendForm('service_0k7yua7', 'template_0qcmeep', formRef.current, 'EA-3RrG_w1F1m5nHq')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
+        .then((result) => {
+            document.getElementById("send_button").innerText = "Thank you!"
+            document.getElementById("send_button").classList.remove("loading");
+            setTimeout(() => {
+                closeForm();
+            }, 1200)
+        }, (error) => {
+            console.log(error.text);
+            document.getElementById("send_button").removeAttribute("disabled")
             });
     }
 
@@ -41,7 +49,7 @@ export default function ContactForm({ setContactForm }) {
                     <textarea name="message" id="message" required ></textarea>
                 </label>
                 <button id="close_button" onClick={closeForm}><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256" fill="#000000" xmlns:v="https://vecta.io/nano"><path d="M39.52 32.16l-7.36 7.36L120.64 128l-88.48 88.48 7.36 7.36L128 135.36l88.48 88.48 7.36-7.36L135.36 128l88.48-88.48-7.36-7.36L128 120.64z" fill="#fff" /></svg></button>
-                <button id="send_button">Submit</button>
+                <button id="send_button">Send</button>
             </form>
         </section>)
 };
