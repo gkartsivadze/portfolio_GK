8cd8b678ae70647ad4a3cc86898dc54dfaa78a99
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-08-03 20:14:27 +0400

    rearranged project. Project images are now imported from files

--- src/components/ProjectBlock.jsx
+++ src/components/ProjectBlock.jsx
@@ -1,8 +1,9 @@
 import React from "react"
 
-export default function ProjectBlock({liveLink, repoLink}) {
+export default function ProjectBlock({liveLink, repoLink, imgSrc}) {
     return (
         <div className="project_box">
+            <img src={imgSrc} alt="" />
             <a target="_blank" href={liveLink}>Live</a>
             <a target="_blank" href={repoLink}>Repo</a>
         </div>
--- src/directory/Projects.jsx
+++ src/directory/Projects.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import useResize from "../hooks/useResize";
 import LookoutAnimation from "../hooks/LookoutAnimation";
 
 import ProjectBlock from "../components/ProjectBlock"
 import Loading from "../components/Loading"
+import artopiaImage from "../../public/artopia.png"
 
 import { projects } from "../projects.json"
 
 export default function Projects() {
     const windowWidth = useResize();
     const animation = LookoutAnimation( windowWidth );
-    
+    const [loadedImagesCounter, setLoadedImagesCounter] = useState(0);
     return (
         <section id="projects_section">
             <Loading />
             <div className="projects_wrapper">
                 {
-                    projects.map(data => <ProjectBlock key={data.id} liveLink={data.liveUrl} repoLink={data.repoUrl} />)
+                    projects.map(data => <ProjectBlock loadedEventListener={setLoadedImagesCounter} key={data.id} imgSrc={data.imgSrc} liveLink={data.liveUrl} repoLink={data.repoUrl} />)
                 }
             </div>
         </section>
