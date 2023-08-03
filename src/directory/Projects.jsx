05d1675df07c7bacdb56a2212433147328cc9c3d
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-08-03 21:27:13 +0400

    fixed hopefully

--- src/directory/Projects.jsx
+++ src/directory/Projects.jsx
@@ -10,22 +10,24 @@ import { projects } from "../projects.json"
 
 export default function Projects() {
     const windowWidth = useResize();
-    const animation = LookoutAnimation( windowWidth );
+    const animation = LookoutAnimation(windowWidth);
     const [loadedImagesCounter, setLoadedImagesCounter] = useState(0);
     const [loadingState, setLoadingState] = useState(true);
     useEffect(() => {
-        if(loadedImagesCounter == 9) {
+        if (loadedImagesCounter == 9) {
             setLoadingState(false)
         }
-    },[loadedImagesCounter])
+    }, [loadedImagesCounter])
     return (
-        <section id="projects_section" className={loadingState ? "hidden" : undefined}>
+        <>
             {loadingState && <Loading />}
-            <div className="projects_wrapper">
-                {
-                    projects.map(data => <ProjectBlock loadedEventListener={setLoadedImagesCounter} key={data.id} imgSrc={data.imgSrc} liveLink={data.liveUrl} repoLink={data.repoUrl} />)
-                }
-            </div>
-        </section>
+            <section id="projects_section" className={loadingState ? "hidden" : undefined}>
+                <div className="projects_wrapper">
+                    {
+                        projects.map(data => <ProjectBlock loadedEventListener={setLoadedImagesCounter} key={data.id} imgSrc={data.imgSrc} liveLink={data.liveUrl} repoLink={data.repoUrl} />)
+                    }
+                </div>
+            </section>
+        </>
     )
 };
